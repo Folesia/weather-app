c83f8afceb8f931b4bac295da93aeb6ed7c971fd
Folesia <folesia@hotmail.com>
2023-02-13 00:42:56 -0600

    created the HTML, CSS and JS files

---
+++ src/index.js
@@ -0,0 +1,75 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+let now = new Date();
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(now);
+
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "a10a99110c9b74eeb49e3f0430acbc06";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+function getPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "a10a99110c9b74eeb49e3f0430acbc06";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(getPosition);
+}
+
+let searchButton = document.querySelector("#search-button");
+searchButton = addEventListener("submit", handleSubmit);
+
+let currentButton = document.querySelector("#current-button");
+currentButton = addEventListener("submit", getCurrentPosition);
+
+searchCity("Winnipeg");
